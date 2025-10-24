8cbd001f862459da0d7517c390ca4132f4f58d2f
vicky0700018 <vs2734514@gmail.com>
2025-10-24 20:59:27 +0530

    updated

---
+++ app/Models/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'product_name',
+        'product_image',
+        'product_price',
+        'product_description',
+    ];
+}
