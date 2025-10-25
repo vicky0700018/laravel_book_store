4996e1002d0735f1b32be98f1aadf154159762e8
vicky0700018 <vs2734514@gmail.com>
2025-10-26 00:04:59 +0530

    updated

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/product-list', [ProductController::class, 'product_list']);
+
+// There are 3 parts in URL
+// Part 1 :- Which Is Actual URL
+// 2 :- conroller Name
+// 3 :- Thiird One Is function Name
