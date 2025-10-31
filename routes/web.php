c054e0b68848771794446e74270ec012bd2b495e
vicky0700018 <vs2734514@gmail.com>
2025-10-31 20:04:00 +0530

    updated

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/product-list', [ProductController::class, 'product_list']);
+// Route::post('/product-store', [ProductController::class, 'product_store']);
+Route::get('/product-store-form', [ProductController::class, 'product_store_form'])->name('product.store.form');
+Route::post('/product-store', [ProductController::class, 'product_store'])->name('product.store');
+Route::delete('/product/{id}', [ProductController::class, 'SoftDeleteproduct'])->name('product.destroy');
+Route::get('/product/{id}/edit', [ProductController::class, 'productedit'])->name('product.edit');
+Route::put('/product/{id}', [ProductController::class, 'productupdate'])->name('product.update');
 
-// There are 3 parts in URL
-// Part 1 :- Which Is Actual URL
-// 2 :- conroller Name
-// 3 :- Thiird One Is function Name
+
+
+// There are 4 parts in URL
+// 1 :- Which Is Actual URL
+// 2 :- controller Name
+// 3 :- Third One Is function Name
+// 4 :- Name which is used to Redirect or Mantion in form tag in any form
